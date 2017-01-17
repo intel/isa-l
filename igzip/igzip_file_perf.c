5d1558046724db600b61fc2bf219b9cb54ce7fbf
Roy Oursler <roy.j.oursler@intel.com>
2017-01-17 11:08:29 -0700

    igzip: Modify stateless_file_perf to test compression levels
    Change-Id: If628e7f949da6862b99cebe6a0a59c6e6d5443e4
    Signed-off-by: Roy Oursler <roy.j.oursler@intel.com>

--- igzip/igzip_file_perf.c
+++ igzip/igzip_file_perf.c
@@ -31,17 +31,29 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <getopt.h>
 #include "igzip_lib.h"
 #include "test.h"
 
 #define BUF_SIZE 1024
-#define MIN_TEST_LOOPS   8
-#define SMALL_INBUF_SIZE (4 * 1024)
+#define MIN_TEST_LOOPS   10
 #ifndef RUN_MEM_SIZE
 # define RUN_MEM_SIZE 500000000
 #endif
 
 struct isal_zstream stream;
+
+int usage(void)
+{
+	fprintf(stderr,
+		"Usage: igzip_file_perf [options] <infile>\n"
+		"  -h        help\n"
+		"  -X        use compression level X with 0 <= X <= 1\n"
+		"  -b <size> input buffer size, 0 buffers all the input\n"
+		"  -i <iter> number of iterations (at least 1)\n"
+		"  -o <file> output file for compresed data\n");
+	exit(0);
+}
 
 int get_filesize(FILE * f)
 {
@@ -57,49 +69,74 @@ int get_filesize(FILE * f)
 int main(int argc, char *argv[])
 {
 	FILE *in, *out = NULL;
-	unsigned char *inbuf, *outbuf;
-	int i, infile_size, iterations, outbuf_size;
+	unsigned char *inbuf, *outbuf, *level_buf = NULL;
+	int i, c, infile_size, iterations = 0, outbuf_size, inbuf_size = 0;
 	struct isal_huff_histogram histogram;
 	struct isal_hufftables hufftables_custom;
+	int level = 0, level_size = 0, avail_in;
+	char *in_file_name = NULL, *out_file_name = NULL;
 
-	memset(&histogram, 0, sizeof(histogram));
-
-	if (argc > 3 || argc < 2) {
-		fprintf(stderr, "Usage: igzip_file_perf  infile [outfile]\n"
-			"\t - Runs multiple iterations of igzip on a file to "
-			"get more accurate time results.\n");
-		exit(0);
+	while ((c = getopt(argc, argv, "h01i:b:o:")) != -1) {
+		switch (c) {
+		case 'o':
+			out_file_name = optarg;
+			break;
+		case 'i':
+			iterations = atoi(optarg);
+			if (iterations < 1)
+				usage();
+			break;
+		case 'b':
+			inbuf_size = atoi(optarg);
+			break;
+		case '1':
+			level = 1;
+			level_size = ISAL_DEF_LVL1_LARGE;
+			break;
+		case '0':
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+		}
 	}
-	in = fopen(argv[1], "rb");
+
+	if (optind < argc) {
+		in_file_name = argv[optind];
+		in = fopen(in_file_name, "rb");
+	} else
+		usage();
+
 	if (!in) {
-		fprintf(stderr, "Can't open %s for reading\n", argv[1]);
+		fprintf(stderr, "Can't open %s for reading\n", in_file_name);
 		exit(0);
 	}
-	if (argc > 2) {
-		out = fopen(argv[2], "wb");
+	if (out_file_name != NULL) {
+		out = fopen(out_file_name, "wb");
 		if (!out) {
-			fprintf(stderr, "Can't open %s for writing\n", argv[2]);
+			fprintf(stderr, "Can't open %s for writing\n", out_file_name);
 			exit(0);
 		}
-		printf("outfile=%s\n", argv[2]);
+		printf("outfile=%s\n", out_file_name);
 	}
-	printf("Window Size: %d K\n", IGZIP_HIST_SIZE);
+
+	printf("Window Size: %d K\n", IGZIP_HIST_SIZE / 1024);
 	printf("igzip_file_perf: \n");
 	fflush(0);
+
 	/* Allocate space for entire input file and output
 	 * (assuming some possible expansion on output size)
 	 */
 	infile_size = get_filesize(in);
 
-	if (infile_size != 0) {
-		outbuf_size = infile_size * 2;
-		iterations = RUN_MEM_SIZE / infile_size;
-	} else {
-		outbuf_size = BUF_SIZE;
-		iterations = MIN_TEST_LOOPS;
+	outbuf_size = 2 * infile_size + BUF_SIZE;
+
+	if (iterations == 0) {
+		iterations = infile_size ? RUN_MEM_SIZE / infile_size : MIN_TEST_LOOPS;
+		if (iterations < MIN_TEST_LOOPS)
+			iterations = MIN_TEST_LOOPS;
 	}
-	if (iterations < MIN_TEST_LOOPS)
-		iterations = MIN_TEST_LOOPS;
 
 	inbuf = malloc(infile_size);
 	if (inbuf == NULL) {
@@ -112,7 +149,17 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	printf("igzip_file_perf: %s %d iterations\n", argv[1], iterations);
+	if (level_size != 0) {
+		level_buf = malloc(level_size);
+		if (level_buf == NULL) {
+			fprintf(stderr, "Can't allocate level buffer memory\n");
+			exit(0);
+		}
+	}
+
+	inbuf_size = inbuf_size ? inbuf_size : infile_size;
+
+	printf("igzip_file_perf: %s %d iterations\n", in_file_name, iterations);
 	/* Read complete input file into buffer */
 	stream.avail_in = (uint32_t) fread(inbuf, 1, infile_size, in);
 	if (stream.avail_in != infile_size) {
@@ -123,21 +170,21 @@ int main(int argc, char *argv[])
 	struct perf start, stop;
 	perf_start(&start);
 
-#ifdef SMALL_IN_BUFFER
-	int avail_in;
 	for (i = 0; i < iterations; i++) {
 		isal_deflate_init(&stream);
 		stream.end_of_stream = 0;
 		stream.flush = NO_FLUSH;
+		stream.level = level;
+		stream.level_buf = level_buf;
+		stream.level_buf_size = level_size;
 		stream.next_out = outbuf;
 		stream.avail_out = outbuf_size;
 		stream.next_in = inbuf;
 		avail_in = infile_size;
 
 		while (avail_in > 0) {
-			stream.avail_in =
-			    avail_in >= SMALL_INBUF_SIZE ? SMALL_INBUF_SIZE : avail_in;
-			avail_in -= SMALL_INBUF_SIZE;
+			stream.avail_in = avail_in >= inbuf_size ? inbuf_size : avail_in;
+			avail_in -= inbuf_size;
 
 			if (avail_in <= 0)
 				stream.end_of_stream = 1;
@@ -149,20 +196,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-#else
-	for (i = 0; i < iterations; i++) {
-		isal_deflate_init(&stream);
-		stream.end_of_stream = 1;	/* Do the entire file at once */
-		stream.flush = NO_FLUSH;
-		stream.next_in = inbuf;
-		stream.avail_in = infile_size;
-		stream.next_out = outbuf;
-		stream.avail_out = outbuf_size;
-		isal_deflate(&stream);
-		if (stream.avail_in != 0)
-			break;
-	}
-#endif
 	perf_stop(&stop);
 
 	if (stream.avail_in != 0) {
@@ -170,23 +203,44 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	printf("  file %s - in_size=%d out_size=%d iter=%d ratio_default=%3.1f%%", argv[1],
-	       infile_size, stream.total_out, i, 100.0 * stream.total_out / infile_size);
+	printf("  file %s - in_size=%d out_size=%d iter=%d ratio=%3.1f%%",
+	       in_file_name, infile_size, stream.total_out, i,
+	       100.0 * stream.total_out / infile_size);
 
-	isal_update_histogram(inbuf, infile_size, &histogram);
-	isal_create_hufftables(&hufftables_custom, &histogram);
+	if (level == 0) {
+		memset(&histogram, 0, sizeof(histogram));
 
-	isal_deflate_init(&stream);
-	stream.end_of_stream = 1;	/* Do the entire file at once */
-	stream.flush = NO_FLUSH;
-	stream.next_in = inbuf;
-	stream.avail_in = infile_size;
-	stream.next_out = outbuf;
-	stream.avail_out = outbuf_size;
-	stream.hufftables = &hufftables_custom;
-	isal_deflate(&stream);
+		isal_update_histogram(inbuf, infile_size, &histogram);
+		isal_create_hufftables(&hufftables_custom, &histogram);
 
-	printf(" ratio_custom=%3.1f%%\n", 100.0 * stream.total_out / infile_size);
+		isal_deflate_init(&stream);
+		stream.end_of_stream = 0;
+		stream.flush = NO_FLUSH;
+		stream.level = level;
+		stream.level_buf = level_buf;
+		stream.level_buf_size = level_size;
+		stream.next_out = outbuf;
+		stream.avail_out = outbuf_size;
+		stream.next_in = inbuf;
+		stream.hufftables = &hufftables_custom;
+		avail_in = infile_size;
+
+		while (avail_in > 0) {
+			stream.avail_in = avail_in >= inbuf_size ? inbuf_size : avail_in;
+			avail_in -= inbuf_size;
+
+			if (avail_in <= 0)
+				stream.end_of_stream = 1;
+
+			isal_deflate(&stream);
+
+			if (stream.avail_in != 0)
+				break;
+		}
+
+		printf(" ratio_custom=%3.1f%%", 100.0 * stream.total_out / infile_size);
+	}
+	printf("\n");
 
 	if (stream.avail_in != 0) {
 		fprintf(stderr, "Could not compress all of inbuf\n");
@@ -197,7 +251,7 @@ int main(int argc, char *argv[])
 	perf_print(stop, start, (long long)infile_size * i);
 
 	if (argc > 2 && out) {
-		printf("writing %s\n", argv[2]);
+		printf("writing %s\n", out_file_name);
 		fwrite(outbuf, 1, stream.total_out, out);
 		fclose(out);
 	}
--- igzip/igzip_stateless_file_perf.c
+++ igzip/igzip_stateless_file_perf.c
@@ -30,6 +30,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <getopt.h>
 #include "igzip_lib.h"
 #include "test.h"
 
@@ -40,6 +42,17 @@
 #endif
 
 struct isal_zstream stream;
+
+int usage(void)
+{
+	fprintf(stderr,
+		"Usage: igzip_stateless_file_perf [options] <infile>\n"
+		"  -h        help\n"
+		"  -X        use compression level X with 0 <= X <= 1\n"
+		"  -i <iter> number of iterations (at least 1)\n"
+		"  -o <file> output file for compresed data\n");
+	exit(0);
+}
 
 int get_filesize(FILE * f)
 {
@@ -55,28 +68,55 @@ int get_filesize(FILE * f)
 int main(int argc, char *argv[])
 {
 	FILE *in, *out = NULL;
-	unsigned char *inbuf, *outbuf;
-	int i, infile_size, iterations, outbuf_size;
+	unsigned char *inbuf, *outbuf, *level_buf = NULL;
+	int i, c, infile_size, iterations = 0, outbuf_size;
+	struct isal_huff_histogram histogram;
+	struct isal_hufftables hufftables_custom;
+	int level = 0, level_size = 0;
+	char *in_file_name = NULL, *out_file_name = NULL;
 
-	if (argc > 3 || argc < 2) {
-		fprintf(stderr, "Usage: igzip_file_perf  infile [outfile]\n"
-			"\t - Runs multiple iterations of igzip on a file to "
-			"get more accurate time results.\n");
-		exit(0);
+	while ((c = getopt(argc, argv, "h01i:o:")) != -1) {
+		switch (c) {
+		case 'o':
+			out_file_name = optarg;
+			break;
+		case 'i':
+			iterations = atoi(optarg);
+			if (iterations < 1)
+				usage();
+			break;
+		case '1':
+			level = 1;
+			level_size = ISAL_DEF_LVL1_LARGE;
+			break;
+		case '0':
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+		}
 	}
-	in = fopen(argv[1], "rb");
+
+	if (optind < argc) {
+		in_file_name = argv[optind];
+		in = fopen(in_file_name, "rb");
+	} else
+		usage();
+
 	if (!in) {
-		fprintf(stderr, "Can't open %s for reading\n", argv[1]);
+		fprintf(stderr, "Can't open %s for reading\n", in_file_name);
 		exit(0);
 	}
-	if (argc > 2) {
-		out = fopen(argv[2], "wb");
+	if (out_file_name != NULL) {
+		out = fopen(out_file_name, "wb");
 		if (!out) {
-			fprintf(stderr, "Can't open %s for writing\n", argv[2]);
+			fprintf(stderr, "Can't open %s for writing\n", out_file_name);
 			exit(0);
 		}
-		printf("outfile=%s\n", argv[2]);
+		printf("outfile=%s\n", out_file_name);
 	}
+
 	printf("Window Size: %d K\n", IGZIP_HIST_SIZE / 1024);
 	printf("igzip_file_perf: \n");
 	fflush(0);
@@ -85,15 +125,13 @@ int main(int argc, char *argv[])
 	 */
 	infile_size = get_filesize(in);
 
-	if (infile_size != 0) {
-		outbuf_size = infile_size * 1.07;
-		iterations = RUN_MEM_SIZE / infile_size;
-	} else {
-		outbuf_size = BUF_SIZE;
-		iterations = MIN_TEST_LOOPS;
+	outbuf_size = infile_size * 1.07 + BUF_SIZE;
+
+	if (iterations == 0) {
+		iterations = infile_size ? RUN_MEM_SIZE / infile_size : MIN_TEST_LOOPS;
+		if (iterations < MIN_TEST_LOOPS)
+			iterations = MIN_TEST_LOOPS;
 	}
-	if (iterations < MIN_TEST_LOOPS)
-		iterations = MIN_TEST_LOOPS;
 
 	inbuf = malloc(infile_size);
 	if (inbuf == NULL) {
@@ -106,7 +144,15 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	printf("igzip_file_perf: %s %d iterations\n", argv[1], iterations);
+	if (level_size != 0) {
+		level_buf = malloc(level_size);
+		if (level_buf == NULL) {
+			fprintf(stderr, "Can't allocate level buffer memory\n");
+			exit(0);
+		}
+	}
+
+	printf("igzip_file_perf: %s %d iterations\n", in_file_name, iterations);
 	/* Read complete input file into buffer */
 	stream.avail_in = (uint32_t) fread(inbuf, 1, infile_size, in);
 	if (stream.avail_in != infile_size) {
@@ -125,6 +171,9 @@ int main(int argc, char *argv[])
 		stream.avail_in = infile_size;
 		stream.next_out = outbuf;
 		stream.avail_out = outbuf_size;
+		stream.level = level;
+		stream.level_buf = level_buf;
+		stream.level_buf_size = level_size;
 		isal_deflate_stateless(&stream);
 		if (stream.avail_in != 0)
 			break;
@@ -136,14 +185,37 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	printf("  file %s - in_size=%d out_size=%d iter=%d ratio=%3.1f%%\n", argv[1],
+	printf("  file %s - in_size=%d out_size=%d iter=%d ratio=%3.1f%%", in_file_name,
 	       infile_size, stream.total_out, i, 100.0 * stream.total_out / infile_size);
+
+	if (level == 0) {
+		memset(&histogram, 0, sizeof(histogram));
+
+		isal_update_histogram(inbuf, infile_size, &histogram);
+		isal_create_hufftables(&hufftables_custom, &histogram);
+
+		isal_deflate_init(&stream);
+		stream.end_of_stream = 1;	/* Do the entire file at once */
+		stream.flush = NO_FLUSH;
+		stream.next_in = inbuf;
+		stream.avail_in = infile_size;
+		stream.next_out = outbuf;
+		stream.avail_out = outbuf_size;
+		stream.level = level;
+		stream.level_buf = level_buf;
+		stream.level_buf_size = level_size;
+		stream.hufftables = &hufftables_custom;
+		isal_deflate_stateless(&stream);
+
+		printf(" ratio_custom=%3.1f%%", 100.0 * stream.total_out / infile_size);
+	}
+	printf("\n");
 
 	printf("igzip_file: ");
 	perf_print(stop, start, (long long)infile_size * i);
 
 	if (argc > 2 && out) {
-		printf("writing %s\n", argv[2]);
+		printf("writing %s\n", out_file_name);
 		fwrite(outbuf, 1, stream.total_out, out);
 		fclose(out);
 	}
