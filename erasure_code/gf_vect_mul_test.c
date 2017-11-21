cb4cea60daad4ab30571357e8dfe862a8a7f5e83
Greg Tucker <greg.b.tucker@intel.com>
2017-11-21 16:54:53 -0700

    test: Remove redundant arch-specific tests
    Change-Id: Ifdbac9d8a99888bfd7a12da5d47dd07b8f85481d
    Signed-off-by: Greg Tucker <greg.b.tucker@intel.com>

--- erasure_code/gf_vect_mul_test.c
+++ erasure_code/gf_vect_mul_test.c
@@ -29,13 +29,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>		// for memset
 #include "erasure_code.h"
 
-#define TEST_SIZE 8192
-#define TEST_MEM  TEST_SIZE
-#define TEST_LOOPS 100000
-#define TEST_TYPE_STR ""
+#define TEST_SIZE (128*1024)
 
 typedef unsigned char u8;
 
@@ -43,12 +39,13 @@ int main(int argc, char *argv[])
 {
 	int i;
 	u8 *buff1, *buff2, *buff3, gf_const_tbl[64], a = 2;
+	int tsize;
 	int align, size;
 	unsigned char *efence_buff1;
 	unsigned char *efence_buff2;
 	unsigned char *efence_buff3;
 
-	printf("gf_vect_mul_test:\n");
+	printf("gf_vect_mul_test: ");
 
 	gf_vect_mul_init(a, gf_const_tbl);
 
@@ -66,40 +63,58 @@ int main(int argc, char *argv[])
 
 	gf_vect_mul(TEST_SIZE, gf_const_tbl, buff1, buff2);
 
-	for (i = 0; i < TEST_SIZE; i++)
+	for (i = 0; i < TEST_SIZE; i++) {
 		if (gf_mul(a, buff1[i]) != buff2[i]) {
-			printf("fail at %d, 0x%x x 2 = 0x%x (0x%x)\n", i, buff1[i], buff2[i],
-			       gf_mul(2, buff1[i]));
-			return 1;
+			printf("fail at %d, 0x%x x 2 = 0x%x (0x%x)\n", i,
+			       buff1[i], buff2[i], gf_mul(2, buff1[i]));
+			return -1;
 		}
+	}
 
 	gf_vect_mul_base(TEST_SIZE, gf_const_tbl, buff1, buff3);
 
 	// Check reference function
-	for (i = 0; i < TEST_SIZE; i++)
+	for (i = 0; i < TEST_SIZE; i++) {
 		if (buff2[i] != buff3[i]) {
 			printf("fail at %d, 0x%x x 0x%d = 0x%x (0x%x)\n",
 			       i, a, buff1[i], buff2[i], gf_mul(a, buff1[i]));
-			return 1;
+			return -1;
 		}
+	}
 
 	for (i = 0; i < TEST_SIZE; i++)
 		buff1[i] = rand();
 
 	// Check each possible constant
-	printf("Random tests ");
 	for (a = 0; a != 255; a++) {
 		gf_vect_mul_init(a, gf_const_tbl);
 		gf_vect_mul(TEST_SIZE, gf_const_tbl, buff1, buff2);
 
-		for (i = 0; i < TEST_SIZE; i++) {
+		for (i = 0; i < TEST_SIZE; i++)
 			if (gf_mul(a, buff1[i]) != buff2[i]) {
 				printf("fail at %d, 0x%x x %d = 0x%x (0x%x)\n",
 				       i, a, buff1[i], buff2[i], gf_mul(2, buff1[i]));
-				return 1;
+				return -1;
 			}
-		}
 		putchar('.');
+	}
+
+	// Check buffer len
+	for (tsize = TEST_SIZE; tsize > 0; tsize -= 32) {
+		a = rand();
+		gf_vect_mul_init(a, gf_const_tbl);
+		gf_vect_mul(tsize, gf_const_tbl, buff1, buff2);
+
+		for (i = 0; i < tsize; i++)
+			if (gf_mul(a, buff1[i]) != buff2[i]) {
+				printf("fail at %d, 0x%x x %d = 0x%x (0x%x)\n",
+				       i, a, buff1[i], buff2[i], gf_mul(2, buff1[i]));
+				return -1;
+			}
+		if (0 == tsize % (32 * 8)) {
+			putchar('.');
+			fflush(0);
+		}
 	}
 
 	// Run tests at end of buffer for Electric Fence
@@ -138,5 +153,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf(" done: Pass\n");
+	fflush(0);
 	return 0;
 }
