0ad8ea9a156f4c86c07db7eeef7b647e9f538aee
Greg Tucker <greg.b.tucker@intel.com>
2018-06-15 15:54:59 -0700

    test: Fix compile warning in dump inflate corpus for fuzz
    Change-Id: I2734ec46687c5188a207963c6631bb4280c1fb8f
    Signed-off-by: Greg Tucker <greg.b.tucker@intel.com>

--- tests/fuzz/igzip_dump_inflate_corpus.c
+++ tests/fuzz/igzip_dump_inflate_corpus.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include "inflate_std_vects.h"
 
-#define FNAME_MAX 180
+#define DNAME_MAX 256
+#define FNAME_MAX (DNAME_MAX + 81)
 
 int main(int argc, char *argv[])
 {
@@ -11,20 +12,20 @@ int main(int argc, char *argv[])
 	int i, len, err;
 	FILE *fout = NULL;
 	char fname[FNAME_MAX];
-	char dname[FNAME_MAX];
+	char dname[DNAME_MAX];
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <outdir>\n", argv[0]);
 		exit(1);
 	}
-	strncpy(dname, argv[1], FNAME_MAX);
+	strncpy(dname, argv[1], DNAME_MAX - 1);
 
 	for (i = 0; i < sizeof(std_vect_array) / sizeof(struct vect_result); i++) {
 		buf = std_vect_array[i].vector;
 		len = std_vect_array[i].vector_length;
 		err = std_vect_array[i].expected_error;
 
-		snprintf(fname, FNAME_MAX, "%s/inflate_corp_n%03d_e%d", dname, i, err);
+		snprintf(fname, FNAME_MAX, "%s/inflate_corp_n%04d_e%04d", dname, i, err);
 		printf(" writing %s\n", fname);
 		fout = fopen(fname, "w+");
 		if (!fout) {
