bad3a0af87e92236cf1cdc4a2a4b445a974a39c9
Greg Tucker <greg.b.tucker@intel.com>
2018-08-08 14:00:59 -0700

    test: Add build info to autotest output
    Change-Id: I6a8fdd3c00f7b598c91ccf2b6d96507da164a991
    Signed-off-by: Greg Tucker <greg.b.tucker@intel.com>

--- tools/test_autorun.sh
+++ tools/test_autorun.sh
@@ -6,10 +6,36 @@ set -e #exit on fail
 READLINK=readlink
 command -V greadlink >/dev/null 2>&1 && READLINK=greadlink
 
-
+# Run in build directory
 out="$PWD"
 src=$($READLINK -f $(dirname $0))/..
 cd "$src"
+
+# Echo environment info
+if test -d .git; then
+    branch=$(git describe)
+    commitid=$(git rev-parse HEAD)
+    brief=$(git log -1 --format='%s')
+    branch_changes=$(git diff --shortstat)
+fi
+if command -V uname >/dev/null 2>&1; then
+    node=$(uname -n)
+    os_name=$(uname -s)
+    os_all=$(uname -a)
+fi
+
+echo "Test report v1"
+echo "branch:              $branch"
+echo "brief:               $brief"
+echo "commitid:            $commitid"
+echo "node:                $node"
+echo "os_name:             $os_name"
+echo "os_all:              $os_all"
+echo "test_args:           $@"
+echo "changes:             $branch_changes"
+command -V lscpu > /dev/null 2>&1 && lscpu
+
+# Start tests
 
 [ -z "$1" ] && ./tools/test_checks.sh
 
