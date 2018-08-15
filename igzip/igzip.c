intel/isa-l

commit 2a292689eceb5ebc87b4414bba4dc3eb93641dd9

igzip: Fix deflate for large buffers
Change-Id: I1993e0a6d3aa36c68af80229329316b2e0616a09
Signed-off-by: Roy Oursler <roy.j.oursler@intel.com>

igzip/igzip.c
@@ -1273,7 +1273,7 @@ int isal_deflate(struct isal_zstream *stream)
 
 		stream->flush = flush_type;
 		stream->end_of_stream = end_of_stream;
-	} while (stream->total_in < total_start + hist_size && avail_in > 0
+	} while (stream->total_in - total_start < hist_size && avail_in > 0
 		 && stream->avail_out > 0);
 
 	stream->total_in += state->b_bytes_valid - state->b_bytes_processed;
@@ -1282,7 +1282,7 @@ int isal_deflate(struct isal_zstream *stream)
 
 	if (stream->avail_in > 0 && stream->avail_out > 0) {
 		/* Due to exiting conditions for the while loop, we know that
-		 * stream->total_in < total_start + hist_size */
+		 * stream->total_in - total_start < hist_size */
 		stream->next_in -= state->b_bytes_valid - state->b_bytes_processed;
 		stream->avail_in += state->b_bytes_valid - state->b_bytes_processed;
 		stream->total_in -= state->b_bytes_valid - state->b_bytes_processed;

commit a32245b97bb9fe006995d0cd1945a19223ac3b04

igzip: Fix underflow bug in total_in in isal_deflate()
Fix bug introduced by patch 2a292689e.

Change-Id: If53af716d546e9430eb8aaae32c2f6133aba21a2
Signed-off-by: Roy Oursler <roy.j.oursler@intel.com>

igzip/igzip.c
@@ -1273,8 +1273,8 @@ int isal_deflate(struct isal_zstream *stream)
 
 		stream->flush = flush_type;
 		stream->end_of_stream = end_of_stream;
-	} while (stream->total_in - total_start < hist_size && avail_in > 0
-		 && stream->avail_out > 0);
+	} while ((int32_t) (stream->total_in - total_start) < (int32_t) hist_size
+		 && avail_in > 0 && stream->avail_out > 0);
 
 	stream->total_in += state->b_bytes_valid - state->b_bytes_processed;
 	stream->next_in = next_in;