9e58553bd942b4689f08c9d1b6025416696fb38a
1f604 <git@1f604.com>
2023-05-13 18:11:49 +0100

    Refactor 13: Add random number generator

--- examples/ec/ec_simple_example.c
+++ examples/ec/ec_simple_example.c
@@ -57,6 +57,31 @@ static int gf_gen_decode_matrix_simple(u8 * encode_matrix,
 				       u8 * decode_index,
 				       u8 * frag_err_list, int nerrs, int k, int m);
 
+const unsigned char generate_byte(const unsigned char upper_bound){ // generates numbers in the range [0, upper_bound] - all inclusive!
+    // I have thoroughly tested this function and informally proved that it is correct to my satisfaction.
+    // this function will reject up to half of all generated numbers
+    // this is a bit inefficient, but is ok when speed doesn't matter
+    // you can fairly easily extend this to more than 255 by interpreting multiple bytes as 32 or 64 bit integer
+    // you can also make a batch version, which would be faster
+    const int n = 256; // the number of numbers in our input space
+    assert(upper_bound < n); // the biggest number we can generate, given our input range
+    const int range = upper_bound + 1; // the number of numbers in our output space
+    //printf("range:%u\n", range);
+    unsigned char array[1];
+    const int nearest_multiple = (n - (n % range));
+    assert(nearest_multiple >= n / 2);
+    //printf("nearest multiple: %d\n", nearest_multiple);
+    while (1){
+        int r = getrandom(array, 1, 0); // generate one random byte
+        assert(r == 1); // verify that we got one byte back
+        unsigned char x = array[0];
+        if (x < nearest_multiple){
+            //printf("generated: %u\n", x);
+            return x % range;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
 	int i, j, m, c, ret;
