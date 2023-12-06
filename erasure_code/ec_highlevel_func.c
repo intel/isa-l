164d9ff1f022cb30ef9b46ceee26d2fddb516f3f
Marcel Cornu <marcel.d.cornu@intel.com>
2023-12-06 22:21:33 +0000

    erasure_code: add 2 vector AVX2 dot product with GFNI implementation
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- erasure_code/ec_highlevel_func.c
+++ erasure_code/ec_highlevel_func.c
@@ -269,6 +269,8 @@ extern void gf_6vect_mad_avx512_gfni(int len, int vec, int vec_i, unsigned char 
 
 extern void gf_vect_dot_prod_avx2_gfni(int len, int k, unsigned char *g_tbls,
 				       unsigned char **data, unsigned char *dest);
+extern void gf_2vect_dot_prod_avx2_gfni(int len, int k, unsigned char *g_tbls,
+					unsigned char **data, unsigned char **coding);
 extern void gf_3vect_dot_prod_avx2_gfni(int len, int k, unsigned char *g_tbls,
 					unsigned char **data, unsigned char **coding);
 extern void gf_vect_mad_avx2_gfni(int len, int vec, int vec_i, unsigned char *gftbls,
@@ -327,13 +329,17 @@ void ec_encode_data_avx2_gfni(int len, int k, int rows, unsigned char *g_tbls,
 		coding += 3;
 		rows -= 3;
 	}
-	while (rows) {
+	switch (rows) {
+	case 2:
+		gf_2vect_dot_prod_avx2_gfni(len, k, g_tbls, data, coding);
+		break;
+	case 1:
 		gf_vect_dot_prod_avx2_gfni(len, k, g_tbls, data, *coding);
-		g_tbls += k * 8;
-		coding++;
-		rows--;
+		break;
+	case 0:
+	default:
+		break;
 	}
-
 }
 
 void ec_encode_data_update_avx512_gfni(int len, int k, int rows, int vec_i,
