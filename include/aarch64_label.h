0234d629a4106b2ffa9cc1a6bb6020d8417e3912
Marcel Cornu <marcel.d.cornu@intel.com>
2024-05-02 15:51:50 +0100

    clang-format: ignore aarch64_label.h
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- include/aarch64_label.h
+++ include/aarch64_label.h
@@ -3,16 +3,16 @@
 
 #ifdef __USER_LABEL_PREFIX__
 #define CONCAT1(a, b) CONCAT2(a, b)
-#define CONCAT2(a, b) a##b
-#define cdecl(x)      CONCAT1(__USER_LABEL_PREFIX__, x)
+#define CONCAT2(a, b) a ## b
+#define cdecl(x) CONCAT1 (__USER_LABEL_PREFIX__, x)
 #else
 #define cdecl(x) x
 #endif
 
 #ifdef __APPLE__
-#define ASM_DEF_RODATA .section __TEXT, __const
+#define ASM_DEF_RODATA .section	__TEXT,__const
 #else
-#define ASM_DEF_RODATA .section.rodata
+#define ASM_DEF_RODATA .section .rodata
 #endif
 
 #endif
