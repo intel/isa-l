aed015edf85f643da5ab9d2e4b833d2e3c6ef514
Ji Dong <dong.ji1@zte.com.cn>
2025-07-24 11:35:30 +0800

    crc: add RISC-V implementation
    The CRC module of ISA-L has been accelerated using RISC-V's V, Zbc, Zbb and Zvbc, instruction sets, implementing data folding and Barrett reduction optimizations.
    
    Signed-off-by: Ji Dong <dong.ji1@zte.com.cn>

--- include/riscv64_multibinary.h
+++ include/riscv64_multibinary.h
@@ -108,7 +108,47 @@
 .endm
 #else /* __ASSEMBLY__ */
 #include <sys/auxv.h>
+#if HAVE_SYS_HWPROBE_H
+#include <sys/hwprobe.h>
+#elif HAVE_ASM_HWPROBE_H
+#include <asm/hwprobe.h>
+#endif
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
 #define HWCAP_RV(letter) (1ul << ((letter) - 'A'))
+
+#if HAVE_SYS_HWPROBE_H || HAVE_ASM_HWPROBE_H
+
+	#define INIT_PROBE_STRUCT()    \
+		(struct riscv_hwprobe){    \
+			.key = RISCV_HWPROBE_KEY_IMA_EXT_0 \
+		}
+
+	#if HAVE_ZBC && HAVE_ZBB && HAVE_ZVBC
+	#define EXT_CODE(ext) ( \
+		strcmp(ext, "ZBC")  == 0 ? RISCV_HWPROBE_EXT_ZBC  : \
+		strcmp(ext, "ZBB")  == 0 ? RISCV_HWPROBE_EXT_ZBB  : \
+		strcmp(ext, "ZVBC") == 0 ? RISCV_HWPROBE_EXT_ZVBC : \
+		-1)
+
+	static inline int check_riscv_extensions(const char **extensions, size_t count)
+	{
+		struct riscv_hwprobe _probe = INIT_PROBE_STRUCT();
+		syscall(__NR_riscv_hwprobe, &_probe, 1, 0, NULL, 0);
+		for (size_t i = 0; i < count; i++) {
+			if (!(_probe.value & EXT_CODE(extensions[i]))) {
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+	#define CHECK_RISCV_EXTENSIONS(...) \
+		check_riscv_extensions((const char*[]){ __VA_ARGS__ }, \
+								sizeof((const char*[]){ __VA_ARGS__ })/sizeof(const char*))
+	#endif
+#endif
 
 #define DEFINE_INTERFACE_DISPATCHER(name)                               \
 	void * name##_dispatcher(void)
