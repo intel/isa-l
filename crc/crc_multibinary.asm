61837dd12b505ec6294446b7b9db51672f91e0be
Emma Finn <emma.finn@intel.com>
2026-02-03 15:42:57 +0000

    crc: Clean up multibinary dispatchers.
    There was unnecessary extra dispatcher code for crc32.
    Refactor to use mbin_dispatch_init_clmul.
    
    Signed-off-by: Emma Finn <emma.finn@intel.com>

--- crc/crc_multibinary.asm
+++ crc/crc_multibinary.asm
@@ -31,6 +31,7 @@ default rel
 [bits 64]
 
 %include "reg_sizes.asm"
+%include "multibinary.asm"
 
 extern crc32_iscsi_01
 extern crc32_iscsi_by8_02
@@ -58,170 +59,17 @@ extern crc32_ieee_by16_10
 extern crc32_iscsi_by16_10
 extern crc16_t10dif_by16_10
 
-%include "multibinary.asm"
-
-section .data
-;;; *_mbinit are initial values for *_dispatched; is updated on first call.
-;;; Therefore, *_dispatch_init is only executed on first call.
-
-crc32_ieee_dispatched:
-	dq	crc32_ieee_mbinit
-
-crc16_t10dif_dispatched:
-	dq	crc16_t10dif_mbinit
-
-section .text
-;;;;
-; crc32_ieee multibinary function
-;;;;
-mk_global crc32_ieee, function
-crc32_ieee_mbinit:
-	endbranch
-	call	crc32_ieee_dispatch_init
-crc32_ieee:
-	endbranch
-	jmp	qword [crc32_ieee_dispatched]
-
-crc32_ieee_dispatch_init:
-	push    rax
-	push    rbx
-	push    rcx
-	push    rdx
-	push    rsi
-	push	rdi
-	lea     rsi, [crc32_ieee_base WRT_OPT] ; Default
-
-	mov     eax, 1
-	cpuid
-	mov	ebx, ecx ; save cpuid1.ecx
-	test    ecx, FLAG_CPUID1_ECX_SSE3
-	jz      .crc_ieee_init_done ; use ieee_base
-	test    ecx, FLAG_CPUID1_ECX_CLMUL
-	jz	.crc_ieee_init_done ; use ieee_base
-	lea	rsi, [crc32_ieee_01 WRT_OPT]
-
-	;; Test for XMM_YMM support/AVX
-	test	ecx, FLAG_CPUID1_ECX_OSXSAVE
-	je	.crc_ieee_init_done
-	xor	ecx, ecx
-	xgetbv	; xcr -> edx:eax
-	mov	edi, eax	  ; save xgetvb.eax
-
-	and	eax, FLAG_XGETBV_EAX_XMM_YMM
-	cmp	eax, FLAG_XGETBV_EAX_XMM_YMM
-	jne	.crc_ieee_init_done
-	test	ebx, FLAG_CPUID1_ECX_AVX
-	je	.crc_ieee_init_done
-	lea	rsi, [crc32_ieee_02 WRT_OPT] ; AVX/02 opt
-
-	;; Test for AVX2
-	xor	ecx, ecx
-	mov	eax, 7
-	cpuid
-	test	ebx, FLAG_CPUID7_EBX_AVX2
-	je	.crc_ieee_init_done		; No AVX2 possible
-
-	;; Test for AVX512
-	and	edi, FLAG_XGETBV_EAX_ZMM_OPM
-	cmp	edi, FLAG_XGETBV_EAX_ZMM_OPM
-	jne	.crc_ieee_init_done	  ; No AVX512 possible
-	and	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-	cmp	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-	jne	.crc_ieee_init_done
-
-	and	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-	cmp	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-	lea	rbx, [crc32_ieee_by16_10 WRT_OPT] ; AVX512/10 opt
-	cmove	rsi, rbx
-
-.crc_ieee_init_done:
-	mov     [crc32_ieee_dispatched], rsi
-	pop	rdi
-	pop     rsi
-	pop     rdx
-	pop     rcx
-	pop     rbx
-	pop     rax
-	ret
-
-;;;;
-; crc16_t10dif multibinary function
-;;;;
-mk_global crc16_t10dif, function
-crc16_t10dif_mbinit:
-	endbranch
-	call	crc16_t10dif_dispatch_init
-crc16_t10dif:
-	endbranch
-	jmp	qword [crc16_t10dif_dispatched]
-
-crc16_t10dif_dispatch_init:
-	push    rax
-	push    rbx
-	push    rcx
-	push    rdx
-	push    rsi
-	push    rdi
-	lea     rsi, [crc16_t10dif_base WRT_OPT] ; Default
-
-	mov     eax, 1
-	cpuid
-	mov	ebx, ecx ; save cpuid1.ecx
-	test    ecx, FLAG_CPUID1_ECX_SSE3
-	jz      .t10dif_init_done ; use t10dif_base
-	test    ecx, FLAG_CPUID1_ECX_CLMUL
-	jz	.t10dif_init_done ; use t10dif_base
-	lea	rsi, [crc16_t10dif_01 WRT_OPT]
-
-	;; Test for XMM_YMM support/AVX
-	test	ecx, FLAG_CPUID1_ECX_OSXSAVE
-	je	.t10dif_init_done
-	xor	ecx, ecx
-	xgetbv	; xcr -> edx:eax
-	mov	edi, eax	  ; save xgetvb.eax
-
-	and	eax, FLAG_XGETBV_EAX_XMM_YMM
-	cmp	eax, FLAG_XGETBV_EAX_XMM_YMM
-	jne	.t10dif_init_done
-	test	ebx, FLAG_CPUID1_ECX_AVX
-	je	.t10dif_init_done
-	lea	rsi, [crc16_t10dif_02 WRT_OPT] ; AVX/02 opt
-
-	;; Test for AVX2
-	xor	ecx, ecx
-	mov	eax, 7
-	cpuid
-	test	ebx, FLAG_CPUID7_EBX_AVX2
-	je	.t10dif_init_done		; No AVX2 possible
-
-	;; Test for AVX512
-	and	edi, FLAG_XGETBV_EAX_ZMM_OPM
-	cmp	edi, FLAG_XGETBV_EAX_ZMM_OPM
-	jne	.t10dif_init_done	  ; No AVX512 possible
-	and	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-	cmp	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-	jne	.t10dif_init_done
-
-	and	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-	cmp	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-	lea	rbx, [crc16_t10dif_by16_10 WRT_OPT] ; AVX512/10 opt
-	cmove	rsi, rbx
-
-.t10dif_init_done:
-	mov     [crc16_t10dif_dispatched], rsi
-	pop     rdi
-	pop     rsi
-	pop     rdx
-	pop     rcx
-	pop     rbx
-	pop     rax
-	ret
-
 mbin_interface			crc32_iscsi
-mbin_dispatch_init_clmul6	crc32_iscsi, crc32_iscsi_base, crc32_iscsi_01, crc32_iscsi_by8_02, crc32_iscsi_avx2, crc32_iscsi_by16_10
+mbin_dispatch_init_clmul	crc32_iscsi, crc32_iscsi_base, crc32_iscsi_01, crc32_iscsi_by8_02, crc32_iscsi_avx2, crc32_iscsi_by16_10
 
 mbin_interface			crc32_gzip_refl
-mbin_dispatch_init_clmul	crc32_gzip_refl, crc32_gzip_refl_base, crc32_gzip_refl_by8, crc32_gzip_refl_by8_02, crc32_gzip_refl_by16_10
+mbin_dispatch_init_clmul	crc32_gzip_refl, crc32_gzip_refl_base, crc32_gzip_refl_by8, crc32_gzip_refl_by8_02, crc32_gzip_refl_by8_02, crc32_gzip_refl_by16_10
 
 mbin_interface			crc16_t10dif_copy
-mbin_dispatch_init_clmul	crc16_t10dif_copy, crc16_t10dif_copy_base, crc16_t10dif_copy_by4, crc16_t10dif_copy_by4_02, crc16_t10dif_copy_by4_02
+mbin_dispatch_init_clmul	crc16_t10dif_copy, crc16_t10dif_copy_base, crc16_t10dif_copy_by4, crc16_t10dif_copy_by4_02, crc16_t10dif_copy_by4_02, crc16_t10dif_copy_by4_02
+
+mbin_interface			crc32_ieee
+mbin_dispatch_init_clmul	crc32_ieee, crc32_ieee_base, crc32_ieee_01, crc32_ieee_02, crc32_ieee_02, crc32_ieee_by16_10
+
+mbin_interface			crc16_t10dif
+mbin_dispatch_init_clmul	crc16_t10dif, crc16_t10dif_base, crc16_t10dif_01, crc16_t10dif_02, crc16_t10dif_02, crc16_t10dif_by16_10
