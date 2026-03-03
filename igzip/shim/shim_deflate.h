9d43b77fd5986aa26706cb400ba3c55a61c22298
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2026-03-03 11:14:24 +0000

    igzip/shim: add Copyright from zlib
    Added Copyright from zlib library as there is code (mainly some
    definitions of structures) coming from this library.
    
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- igzip/shim/shim_deflate.h
+++ igzip/shim/shim_deflate.h
@@ -27,6 +27,31 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **********************************************************************/
 
+/**********************************************************************
+  -- Part of the code below contains structures from zlib.h --
+
+  Copyright (C) 1995-2026 Jean-loup Gailly and Mark Adler
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jean-loup Gailly        Mark Adler
+  jloup@gzip.org          madler@alumni.caltech.edu
+**********************************************************************/
+
 typedef struct {
         void *next_in;          /* next input byte */
         unsigned int avail_in;  /* number of bytes available at next_in */
