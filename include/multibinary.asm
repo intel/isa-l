2a5759ad1d3a52ef37c152e6518fc6ffbd9c1af7
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2026-08-18 09:16:40 +0000

    include: remove unused macros
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- include/multibinary.asm
+++ include/multibinary.asm
@@ -30,15 +30,6 @@
 %ifndef _MULTIBINARY_ASM_
 %define _MULTIBINARY_ASM_
 
-%define dq	dq
-%define ptr_sz	qword
-%define rdi	rdi
-%define rsi	rsi
-%define rax	rax
-%define rbx	rbx
-%define rcx	rcx
-%define rdx	rdx
-
 ;;;;
 ; multibinary macro:
 ;   creates the visible entry point that uses HW optimized call pointer
@@ -66,57 +57,6 @@
 		jmp	qword [%1_dispatched]
 %endmacro
 
-;;;;;
-; mbin_dispatch_init parameters
-; Use this function when SSE/00/01 is a minimum requirement
-; 1-> function name
-; 2-> SSE/00/01 optimized function used as base
-; 3-> AVX or AVX/02 opt func
-; 4-> AVX2 or AVX/04 opt func
-;;;;;
-%macro mbin_dispatch_init 4
-	section .text
-	%1_dispatch_init:
-		push	rsi
-		push	rax
-		push	rbx
-		push	rcx
-		push	rdx
-		lea	rsi, [%2 WRT_OPT] ; Default to SSE 00/01
-
-		mov	eax, 1
-		cpuid
-		and	ecx, (FLAG_CPUID1_ECX_AVX | FLAG_CPUID1_ECX_OSXSAVE)
-		cmp	ecx, (FLAG_CPUID1_ECX_AVX | FLAG_CPUID1_ECX_OSXSAVE)
-		lea	rbx, [%3 WRT_OPT] ; AVX (gen2) opt func
-		jne	_%1_init_done ; AVX is not available so end
-		mov	rsi, rbx
-
-		;; Try for AVX2
-		xor	ecx, ecx
-		mov	eax, 7
-		cpuid
-		test	ebx, FLAG_CPUID7_EBX_AVX2
-		lea	rbx, [%4 WRT_OPT] ; AVX (gen4) opt func
-		cmovne	rsi, rbx
-
-		;; Does it have xmm and ymm support
-		xor	ecx, ecx
-		xgetbv
-		and	eax, FLAG_XGETBV_EAX_XMM_YMM
-		cmp	eax, FLAG_XGETBV_EAX_XMM_YMM
-		je	_%1_init_done
-		lea	rsi, [%2 WRT_OPT]
-
-	_%1_init_done:
-		pop	rdx
-		pop	rcx
-		pop	rbx
-		pop	rax
-		mov	[%1_dispatched], rsi
-		pop	rsi
-		ret
-%endmacro
 ;;;;;
 ; mbin_dispatch_init_clmul 6 parameters
 ; Use this case for CRC which needs both SSE4_1 and CLMUL
@@ -309,81 +249,6 @@
 		and	ebx, FLAGS_CPUID7_EBX_AVX512_G1
 		cmp	ebx, FLAGS_CPUID7_EBX_AVX512_G1
 		lea	rbx, [%6 WRT_OPT] ; AVX512/06 opt
-		cmove	rsi, rbx
-
-	_%1_init_done:
-		pop	rdi
-		pop	rdx
-		pop	rcx
-		pop	rbx
-		pop	rax
-		mov	[%1_dispatched], rsi
-		pop	rsi
-		ret
-%endmacro
-
-;;;;;
-; mbin_dispatch_init7 parameters
-; 1-> function name
-; 2-> base function
-; 3-> SSE4_2 or 00/01 optimized function
-; 4-> AVX/02 opt func
-; 5-> AVX2/04 opt func
-; 6-> AVX512/06 opt func
-; 7-> AVX512 Update/10 opt func
-;;;;;
-%macro mbin_dispatch_init7 7
-	section .text
-	%1_dispatch_init:
-		push	rsi
-		push	rax
-		push	rbx
-		push	rcx
-		push	rdx
-		push	rdi
-		lea	rsi, [%2 WRT_OPT] ; Default - use base function
-
-		mov	eax, 1
-		cpuid
-		mov	ebx, ecx ; save cpuid1.ecx
-		test	ecx, FLAG_CPUID1_ECX_SSE4_2
-		je	_%1_init_done	  ; Use base function if no SSE4_2
-		lea	rsi, [%3 WRT_OPT] ; SSE possible so use 00/01 opt
-
-		;; Test for XMM_YMM support/AVX
-		test	ecx, FLAG_CPUID1_ECX_OSXSAVE
-		je	_%1_init_done
-		xor	ecx, ecx
-		xgetbv	; xcr -> edx:eax
-		mov	edi, eax	  ; save xgetvb.eax
-
-		and	eax, FLAG_XGETBV_EAX_XMM_YMM
-		cmp	eax, FLAG_XGETBV_EAX_XMM_YMM
-		jne	_%1_init_done
-		test	ebx, FLAG_CPUID1_ECX_AVX
-		je	_%1_init_done
-		lea	rsi, [%4 WRT_OPT] ; AVX/02 opt
-
-		;; Test for AVX2
-		xor	ecx, ecx
-		mov	eax, 7
-		cpuid
-		test	ebx, FLAG_CPUID7_EBX_AVX2
-		je	_%1_init_done		; No AVX2 possible
-		lea	rsi, [%5 WRT_OPT] 	; AVX2/04 opt func
-
-		;; Test for AVX512
-		and	edi, FLAG_XGETBV_EAX_ZMM_OPM
-		cmp	edi, FLAG_XGETBV_EAX_ZMM_OPM
-		jne	_%1_init_done	  ; No AVX512 possible
-		and	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-		cmp	ebx, FLAGS_CPUID7_EBX_AVX512_G1
-		lea	rbx, [%6 WRT_OPT] ; AVX512/06 opt
-		cmove	rsi, rbx
-
-		and	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-		cmp	ecx, FLAGS_CPUID7_ECX_AVX512_G2
-		lea	rbx, [%7 WRT_OPT] ; AVX512/06 opt
 		cmove	rsi, rbx
 
 	_%1_init_done:
